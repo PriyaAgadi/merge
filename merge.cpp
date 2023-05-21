d701bcead95336aa6d553d974f423229a6b2e854
Priya Agadi <112713514+PriyaAgadi@users.noreply.github.com>
2023-05-21 13:15:35 +0530

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -1,7 +1,5 @@
 void merge(int arr[], int l, int m, int r){
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
+    
     for (int i = 0; i < n1; i++){
         L[i] = arr[l + i];
     }
